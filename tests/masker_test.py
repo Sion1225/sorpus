d8ddb5fc5b139ebaaf3cccf9e31eff317d21e941
Sion1225 <visiopo44@naver.com>
2024-05-31 23:54:22 +0900

    fix_workflow and reset replacer

--- tests/masker_test.py
+++ tests/masker_test.py
@@ -27,6 +27,14 @@ def test_replace_special_words_between_difference_chars():
     )
 
 
+def test_replace_special_words_between_difference_chars_without_spaces():
+    replacer = SpecialWordReplacer(["pineapple"], "apple", "cone")
+    assert (
+        replacer.mask_words("He said the word brown-pineapples while pointing at a nut.")
+        == "He said the word brown-pinecones while pointing at a nut."
+    )
+
+
 def test_replace_special_words_repr():
     replacer = SpecialWordReplacer(["New York", "El Nino"], " ", "•")
     assert (
